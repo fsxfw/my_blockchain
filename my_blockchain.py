69119384b4d423cee6ced685b42a14cfbdc67272
Yuta Morino <yutamorino@Yutas-MacBook-Pro.local>
2018-07-09 13:53:12 +0900

    add new file

---
+++ my_blockchain.py
@@ -0,0 +1,182 @@
+import hashlib
+import json
+from time import time
+from urllib.parse import urlparse
+from uuid import uuid4
+
+import requests
+from flask import Flask, jsonify, request
+
+
+class Blockchain:
+    def __init__(self,):
+        self.chain = []
+        self.current_transactions = []
+        self.nodes = set()
+        self.create_new_block(previous_hash=1, nonce=0)
+
+    def register_node(self, address):
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+
+    def create_new_block(self, nonce, previous_hash):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'nonce': nonce,
+            'previous_hash': previous_hash,
+        }
+        self.current_transactions = []
+        self.chain.append(block)
+        return block
+
+    def create_new_transaction(self, sender, recipient, amount):
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+        return self.last_block['index'] + 1
+
+    def last_block(self):
+        return self.chain[-1]
+
+    @staticmethod
+    def hash(block):
+        block_string = json.dump(block, sort_keys=True)
+        return hashlib.sha256(block_string.encode()).hexdigest()
+
+    def proof_of_work(self, last_block):
+        nonce = 0
+        while self.valid_proof(last_block, nonce) is False:
+            nonce += 1
+        return nonce
+
+    @staticmethod
+    def valid_proof(last_block, nonce):
+        nonce_joined = hash(last_block) + nonce
+        calced = hashlib.sha256(nonce_joined.encode()).hexdigest()
+        return calced[:4] == "0000"
+
+    def valid_chain(self, chain):
+        last_block = chain[0]
+        current_index = 1
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f'{last_block}')
+            print(f'{block}')
+            print(f'\n-------------\n')
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+            if not self.valid_proof(last_block, block):
+                return False
+        return True
+
+    def resolve_conflicts(self):
+        """
+        これがコンセンサスアルゴリズムだ。ネットワーク上の最も長いチェーンで自らのチェーンを
+        置き換えることでコンフリクトを解消する。
+        :return: <bool> 自らのチェーンが置き換えられると True 、そうでなれけば False
+        """
+
+        neighbours = self.nodes
+        new_chain = None
+
+        # 自らのチェーンより長いチェーンを探す必要がある
+        max_length = len(self.chain)
+
+        # 他のすべてのノードのチェーンを確認
+        for node in neighbours:
+            response = requests.get(f'http://{node}/chain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                # そのチェーンがより長いか、有効かを確認
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        # もし自らのチェーンより長く、かつ有効なチェーンを見つけた場合それで置き換える
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
+
+
+app = Flask(__name__)
+
+node_identifire = str(uuid4()).replace('-', '')
+
+blockchain = Blockchain()
+
+
+# メソッドはPOSTで/transactions/newエンドポイントを作る。メソッドはPOSTなのでデータを送信する
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    values = request.get_json()
+
+    # POSTされたデータに必要なデータがあるかを確認
+    required = ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    # 新しいトランザクションを作る
+    index = blockchain.create_new_transaction(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'トランザクションはブロック {index} に追加されました'}
+    return jsonify(response), 201
+
+
+# メソッドはGETで、フルのブロックチェーンをリターンする/chainエンドポイントを作る
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+
+@app.route('/nodes/register', methods=['POST'])
+def register_node():
+    values = request.get_json()
+
+    nodes = values.get('nodes')
+    if nodes is None:
+        return "Error: 有効ではないノードのリストです", 400
+
+    for node in nodes:
+        blockchain.register_node(node)
+
+    response = {
+        'message': '新しいノードが追加されました',
+        'total_nodes': list(blockchain.nodes),
+    }
+    return jsonify(response), 201
+
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
+
+    if replaced:
+        response = {
+            'message': 'チェーンが置き換えられました',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'チェーンが確認されました',
+            'chain': blockchain.chain
+        }
+
+    return jsonify(response), 200
+
+
+# port5000でサーバーを起動する
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
